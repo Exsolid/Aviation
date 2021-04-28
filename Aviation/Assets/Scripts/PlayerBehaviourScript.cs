e46bf075b4585f089dfd3739928d2e455549a6bd
IgnisLibra <81675005+IgnisLibra@users.noreply.github.com>
2021-04-28 15:51:13 +0200

    Modified Player Script
    Player asset stays inside window borders in play mode.
    started working on a function to make the player "jump".
    Player can use Q to fly up and E to fly down

--- Aviation/Assets/Scripts/PlayerBehaviourScript.cs
+++ Aviation/Assets/Scripts/PlayerBehaviourScript.cs
@@ -17,12 +17,26 @@ public class PlayerBehaviourScript : MonoBehaviour
         /* Eingabe speichern */
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
-        float yInput = Input.GetAxis("TestHeight");
+        float yInput = Input.GetAxis("Height");
 
         /*Neue Position bestimmen */
         float xNew = transform.position.x + xInput * inputFactor * Time.deltaTime;
+        if (xNew < -20f) xNew = -20f;
+        else if (xNew > 20f) xNew = 20f;
         float zNew = transform.position.z + zInput * inputFactor * Time.deltaTime;
+        if (zNew < -10f) zNew = -10f;
+        else if (zNew > 10f) zNew = 10f;
         float yNew = transform.position.y + yInput * inputFactor * Time.deltaTime;
+        if (yNew < 1f) yNew = 1f;
+        else if (yNew > 10f) yNew = 10f; 
+        /* (WIP) float yNew = 1;
         transform.position = new Vector3(xNew, yNew, zNew);
+        if (Input.GetButtonDown("Rise (test)"))
+        {
+            yNew = transform.position.y + 9 * Time.deltaTime;
+            transform.position = new Vector3(xNew, yNew, zNew);
+        }*/
+        transform.position = new Vector3(xNew, yNew, zNew);
+
     }
 }
