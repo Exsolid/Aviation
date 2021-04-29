b0a7695ab5ed20b7db3c8f6535784ed9d33822a4
Samuel Sieberg <ssieberg@exsolid.com>
2021-04-29 20:54:28 +0200

    Enemy shooting and code comments

---
+++ Aviation/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public float Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+    [SerializeField] private float speed;
+    private Rigidbody rb;
+    private float maxDisplayHeightAtGameplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxDisplayHeightAtGameplay = 2.0f * (Mathf.Abs(Camera.main.transform.position.y - transform.position.y)) * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.velocity = new Vector3(0, 0, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Camera.main.transform.position.z + maxDisplayHeightAtGameplay < transform.position.z)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
--- Aviation/Assets/Scripts/EnemyBehaviour.cs
+++ Aviation/Assets/Scripts/EnemyBehaviour.cs
@@ -7,19 +7,29 @@ public class EnemyBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private float speed;
-    [SerializeField] private Rigidbody rb;
     [SerializeField] private float minDistanceToPlayer;
+    [SerializeField] private bool enableRotationOnMove;
+    [SerializeField] private Rigidbody rb;
     private float maxDisplayHeightAtGameplay;
     private float maxDisplayWidthAtGameplay;
     private float xDistanceToPlayer;
     private float zDistanceToPlayer;
 
+    [SerializeField] private Transform gunPosOne;
+    [SerializeField] private Transform gunPosTwo;
+    [SerializeField] private float shootTiming;
+    [SerializeField] private GameObject bulletPrefab;
+    private float shootTimer;
+    private RaycastHit hit;
+
     void Start()
     {
-        speed = Mathf.Abs(speed)*2;
-        float frustumHeight = 2.0f * (Mathf.Abs(Camera.main.transform.position.y - player.transform.position.y)) * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
-        maxDisplayWidthAtGameplay = frustumHeight * Camera.main.aspect; ;
-        maxDisplayHeightAtGameplay = frustumHeight;
+        speed = Mathf.Abs(speed);
+        //Get height and width of the gameplay area (camera view bounds at depth)
+        maxDisplayHeightAtGameplay = 2.0f * (Mathf.Abs(Camera.main.transform.position.y - player.transform.position.y)) * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        maxDisplayWidthAtGameplay = maxDisplayHeightAtGameplay * Camera.main.aspect;
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     void FixedUpdate()
@@ -27,28 +37,60 @@ public class EnemyBehaviour : MonoBehaviour
         if(Camera.main.transform.position.z + maxDisplayHeightAtGameplay > transform.position.z) {
             xDistanceToPlayer = player.transform.position.x - transform.position.x;
             zDistanceToPlayer = player.transform.position.z - transform.position.z;
-            if (zDistanceToPlayer > maxDisplayHeightAtGameplay) zDistanceToPlayer = maxDisplayHeightAtGameplay;
-            tailPlayer();
+            //Creates a raycast (detecting line) that fills 'hit' if it collides with a collider
+            Physics.Raycast(gunPosOne.position, transform.TransformDirection(Vector3.forward), out hit, maxDisplayHeightAtGameplay);
+            shootTimer += Time.deltaTime;
+            move();
+            shoot();
         }
         else
         {
-            gameObject.SetActive(false);
+            //Delete object if out of vision
+            GameObject.Destroy(gameObject);
         }
       
     }
 
-    private void tailPlayer()
+    private void move()
     {
-        rb.velocity = new Vector3(speed * 2 * Mathf.Pow((xDistanceToPlayer / maxDisplayWidthAtGameplay),2) * getDirectionOnX() * Time.deltaTime, 0, Mathf.Pow((zDistanceToPlayer - minDistanceToPlayer) / maxDisplayHeightAtGameplay, 2) * speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, (xDistanceToPlayer / 50) * (xDistanceToPlayer / 50) * 360 * -getDirectionOnX());
+        //               speed * function that returns 0-1 based on the distance to the player                                  * direction to move * framerate edit
+        float speedOnY = speed * 4f * Mathf.Pow(((xDistanceToPlayer + 15 * getDirectionOnX()) / maxDisplayWidthAtGameplay), 2) * getDirectionOnX() * Time.deltaTime;
+        //               speed * function that returns 0-1 based on the distance to the player                                  * direction to move * framerate edit
+        float speedOnZ = speed * Mathf.Pow((zDistanceToPlayer - minDistanceToPlayer) / maxDisplayHeightAtGameplay, 2)  * Time.deltaTime;
+        //Cap speed because zDistanceToPlayer (zDistanceToPlayer - minDistanceToPlayer) / maxDisplayHeightAtGameplay can return big numbers if player to enemy distance gets big
+        if (speedOnZ > speed / 50) speedOnZ = speed / 50;
+        rb.velocity = new Vector3(speedOnY, 0, speedOnZ);
+
+        //                function that returns 0-1 based on the distance to the player 
+        float rotationOnZ = 2*Mathf.Pow((xDistanceToPlayer / maxDisplayWidthAtGameplay),2) * 360 * -getDirectionOnX();
+        if (Mathf.Abs(rotationOnZ) > 45) rotationOnZ = 45 * -getDirectionOnX();
+        if (enableRotationOnMove) transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, rotationOnZ);
     }
 
 
-    private void move(Vector2 direction)
+    private void shoot()
     {
-        rb.velocity = new Vector3(speed * Time.deltaTime, speed * Time.deltaTime, 0) * direction;
+        if (hit.transform != null && shootTimer > shootTiming)
+        {
+            shootTimer = 0;
+            if (gunPosOne != null && bulletPrefab != null)
+            {
+                //Create bullet and add the planes speed to the bullet speed
+                GameObject bullet = GameObject.Instantiate(bulletPrefab, gunPosOne.transform.position, Quaternion.Euler(0, 0, 0));
+                BulletBehaviour behaviour = bullet.GetComponent<BulletBehaviour>();
+                behaviour.Speed += behaviour.Speed + speed * Mathf.Pow((zDistanceToPlayer - minDistanceToPlayer) / maxDisplayHeightAtGameplay, 2) * Time.deltaTime;
+            }
+            if(gunPosTwo != null && bulletPrefab != null)
+            {
+                //Create bullet and add the planes speed to the bullet speed
+                GameObject bullet = GameObject.Instantiate(bulletPrefab, gunPosTwo.transform.position, Quaternion.Euler(0, 0, 0));
+                BulletBehaviour behaviour = bullet.GetComponent<BulletBehaviour>();
+                behaviour.Speed += speed * Mathf.Pow((zDistanceToPlayer - minDistanceToPlayer) / maxDisplayHeightAtGameplay, 2) * Time.deltaTime;
+            }
+        }
     }
 
+    //Returns 1 if the player is on the right, -1 if on the left and 0 if it is close to centered
     private int getDirectionOnX()
     {
         if (player.transform.position.x - transform.position.x > 0.1) return 1;
