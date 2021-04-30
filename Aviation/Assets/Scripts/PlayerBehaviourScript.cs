53bb0f179ee8ed733697c4502c6302fcb9088ead
IgnisLibra <81675005+IgnisLibra@users.noreply.github.com>
2021-04-30 08:53:47 +0200

    Work on PlayerBehaviourScript
    Changed Player asset to Spitfire.
    Added Rigidbody to Player asset

--- Aviation/Assets/Scripts/PlayerBehaviourScript.cs
+++ Aviation/Assets/Scripts/PlayerBehaviourScript.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PlayerBehaviourScript : MonoBehaviour
 {
     readonly float inputFactor = 20;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Rigidbody playerrb;
+    private float maxDisplayHeightAtGameplay;
+    private float maxDisplayWidthAtGameplay;
     // Start is called before the first frame update
     void Start()
     {
-     
+        maxDisplayHeightAtGameplay = 2.0f * (Mathf.Abs(Camera.main.transform.position.y - player.transform.position.y)) * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        maxDisplayWidthAtGameplay = maxDisplayHeightAtGameplay * Camera.main.aspect;
+        playerrb = GetComponent<Rigidbody>();
+        playerrb.useGravity = false;
     }
 
     // Update is called once per frame
@@ -17,26 +24,11 @@ public class PlayerBehaviourScript : MonoBehaviour
         /* Eingabe speichern */
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
-        float yInput = Input.GetAxis("Height");
 
         /*Neue Position bestimmen */
         float xNew = transform.position.x + xInput * inputFactor * Time.deltaTime;
-        if (xNew < -20f) xNew = -20f;
-        else if (xNew > 20f) xNew = 20f;
         float zNew = transform.position.z + zInput * inputFactor * Time.deltaTime;
-        if (zNew < -10f) zNew = -10f;
-        else if (zNew > 10f) zNew = 10f;
-        float yNew = transform.position.y + yInput * inputFactor * Time.deltaTime;
-        if (yNew < 1f) yNew = 1f;
-        else if (yNew > 10f) yNew = 10f; 
-        /* (WIP) float yNew = 1;
-        transform.position = new Vector3(xNew, yNew, zNew);
-        if (Input.GetButtonDown("Rise (test)"))
-        {
-            yNew = transform.position.y + 9 * Time.deltaTime;
-            transform.position = new Vector3(xNew, yNew, zNew);
-        }*/
-        transform.position = new Vector3(xNew, yNew, zNew);
+        transform.position = new Vector3(xNew, 1, zNew);
 
     }
 }
