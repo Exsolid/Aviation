adb48f0bf6101b46081cb864097ed3a1e98dea4a
Samuel Sieberg <ssieberg@exsolid.com>
2021-05-07 19:04:49 +0200

    Adjusted bulltes

--- Aviation/Assets/Scripts/BulletBehaviour.cs
+++ Aviation/Assets/Scripts/BulletBehaviour.cs
@@ -11,22 +11,21 @@ public class BulletBehaviour : MonoBehaviour
     }
     [SerializeField] private float speed;
     private Rigidbody rb;
-    private float maxDisplayHeightAtGameplay;
     // Start is called before the first frame update
     void Start()
     {
-        maxDisplayHeightAtGameplay = 2.0f * (Mathf.Abs(Camera.main.transform.position.y - transform.position.y)) * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
         rb = GetComponent<Rigidbody>();
         rb.useGravity = false;
         rb.velocity = new Vector3(0, 0, speed);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnBecameInvisible()
     {
-        if (Camera.main.transform.position.z + maxDisplayHeightAtGameplay < transform.position.z)
-        {
-            GameObject.Destroy(gameObject);
-        }
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
     }
 }
