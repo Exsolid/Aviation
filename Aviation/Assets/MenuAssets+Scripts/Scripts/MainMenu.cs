7b8aebee8c46068297633334dfddd77c7b5e33c8
GoodOldOak <83174188+GoodOldOak@users.noreply.github.com>
2021-06-23 18:39:35 +0200

    Spitfire-Level vom Menü startbar

--- Aviation/Assets/MenuAssets+Scripts/Scripts/MainMenu.cs
+++ Aviation/Assets/MenuAssets+Scripts/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Muss mit Buildindex angepasst werden. Alternativ LoadScene("*Scenename*");
-        SceneManager.LoadScene("Prototype-Level");
+        SceneManager.LoadScene("Spitfire-Level");
     }
 
     public void GoToSettingsMenu()
